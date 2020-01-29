404038413a2bfaeec383310112da643acfa75751
C-Wwb <1729220020@qq.com>
2020-01-29 20:54:25 +0800

    20.01.29

---
+++ 3.28/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter r1`s x-, y-coordinates, width, and height:");
+        double xCoordinates1 = input.nextDouble(),
+               yCoordinates1 = input.nextDouble(),
+               width1 = input.nextDouble(),
+               height1 = input.nextDouble();
+        System.out.println("Please enter r2`s x-, y-coordinates, width, and height:");
+        double xCoordinates2 = input.nextDouble(),
+                yCoordinates2 = input.nextDouble(),
+                width2 = input.nextDouble(),
+                height2 = input.nextDouble();
+        if()
+
+    }
+}
