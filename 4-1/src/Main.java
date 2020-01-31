94cb9b50d1327d4099c588b378f87d762ab4f5d4
C-Wwb <1729220020@qq.com>
2020-01-31 19:40:32 +0800

    20.01.31

--- 4-1/src/Main.java
+++ 4-1/src/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main
@@ -15,6 +16,10 @@ public class Main
                a = Math.sqrt(Math.pow((y3 - y2),2) + Math.pow((x3 - x2),2)),
                b = Math.sqrt(Math.pow((y3 - y1),2) + Math.pow((x3 - x1),2)),
                c = Math.sqrt(Math.pow((y2 - y1),2) + Math.pow((x2 - x1),2)),
-               A = Math.
+               A = Math.toDegrees(Math.acos((Math.pow(a,2) - Math.pow(b,2) - Math.pow(c,2)) / (-2 * b * c))),
+               B = Math.toDegrees(Math.acos((Math.pow(b,2) - Math.pow(a,2) - Math.pow(c,2)) / (-2 * a * c))),
+               C = Math.toDegrees(Math.acos((Math.pow(c,2) - Math.pow(b,2) - Math.pow(a,2)) / (-2 * a * b)));
+        DecimalFormat d = new DecimalFormat("0.00");
+        System.out.println("The three angles are " + d.format(A) + " " + d.format(B) + " " + d.format(C));
     }
 }
