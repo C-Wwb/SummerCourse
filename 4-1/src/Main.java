b2e7c8d034c71a5118842ea8e7be6330ad39c890
C-Wwb <1729220020@qq.com>
2020-01-31 18:51:50 +0800

    20.01.31

---
+++ 4-1/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter three points:");
+        double x1 = input.nextDouble(),
+               y1 = input.nextDouble(),
+               x2 = input.nextDouble(),
+               y2 = input.nextDouble(),
+               x3 = input.nextDouble(),
+               y3 = input.nextDouble(),
+               a = Math.sqrt(Math.pow((y3 - y2),2) + Math.pow((x3 - x2),2)),
+               b = Math.sqrt(Math.pow((y3 - y1),2) + Math.pow((x3 - x1),2)),
+               c = Math.sqrt(Math.pow((y2 - y1),2) + Math.pow((x2 - x1),2)),
+               A = Math.
+    }
+}
