5bceef34c69b3b7c6fb9590c339f480eb5cd5460
C-Wwb <1729220020@qq.com>
2020-02-02 19:27:28 +0800

    20.02.02

--- 4-5/src/Main.java
+++ 4-5/src/Main.java
@@ -6,8 +6,28 @@ public class Main
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter your lottery pick(two digits):");
-        double lotteryPick = input.nextDouble();
-        String lotteryNumber =" " + (int)(Math.random() * 10) + (int)(Math.random() * 10);
+        String lotteryPick = input.nextLine();
+        String lotteryNumber =(int)(Math.random() * 10 + 10) + "";
         System.out.println("The lottery number is " + lotteryNumber);
+        char   word1 = lotteryPick.charAt(0),
+               word2 = lotteryPick.charAt(1),
+               word3 = lotteryNumber.charAt(0),
+               word4 = lotteryNumber.charAt(1);
+        if(word1 == word3 && word2 == word4)
+        {
+            System.out.println("Exact match: you win $10,000");
+        }
+        else if(word1 == word4 && word2 == word3)
+        {
+            System.out.println("Exact match: you win $3,000");
+        }
+        else if((word1 == word3) || (word1 == word4) || (word2 == word3) || (word2 == word4))
+        {
+            System.out.println("Exact match: you win $1,000");
+        }
+        else
+        {
+            System.out.println("sorry no match");
+        }
     }
 }
