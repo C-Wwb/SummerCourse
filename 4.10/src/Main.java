41b702ecb9e0370c4a916e90fbdc93fca8c5c960
C-Wwb <1729220020@qq.com>
2020-02-04 20:00:06 +0800

    20.02.04

--- 4.10/src/Main.java
+++ 4.10/src/Main.java
@@ -12,34 +12,32 @@ public class Main
                 matrix5 = "16 17 18 19\n" + "20 21 22 23\n" + "24 25 26 27\n" + "28 29 30 31";
         int day = 0;
         System.out.println("Is your birthday in matrix1?\n" + matrix1 + "\nPlease enter N for No and Y for Yes:");
-        String answer1 = input.next(),
-               yes = "Y",
-               no = "N";
-        if(answer1.equalsIgnoreCase(yes))
+        String answer1 = input.next();
+        if(answer1.equalsIgnoreCase("Y"))
         {
             day += 1;
         }
         System.out.println("Is your birthday in matrix2?\n" + matrix2 + "\nPlease enter 0 for No and 1 for Yes:");
         String answer2 = input.next();
-        if(answer2.equalsIgnoreCase(yes))
+        if(answer2.equalsIgnoreCase("Y"))
         {
             day += 2;
         }
         System.out.println("Is your birthday in matrix2?\n" + matrix3 + "\nPlease enter 0 for No and 1 for Yes:");
         String answer3 = input.next();
-        if(answer3.equalsIgnoreCase(yes))
+        if(answer3.equalsIgnoreCase("Y"))
         {
             day += 4;
         }
         System.out.println("Is your birthday in matrix2?\n" + matrix4 + "\nPlease enter 0 for No and 1 for Yes:");
         String answer4 = input.next();
-        if(answer4.equalsIgnoreCase(yes))
+        if(answer4.equalsIgnoreCase("Y"))
         {
             day += 8;
         }
         System.out.println("Is your birthday in matrix2?\n" + matrix5 + "\nPlease enter 0 for No and 1 for Yes:");
         String answer5 = input.next();
-        if(answer5.equalsIgnoreCase(yes))
+        if(answer5.equalsIgnoreCase("Y"))
         {
             day += 16;
         }
