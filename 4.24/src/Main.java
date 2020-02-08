ba7de8db7d12e4fcf2d94503c3f1e5a49b7035d6
C-Wwb <1729220020@qq.com>
2020-02-08 17:18:14 +0800

    20.02.08

---
+++ 4.24/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the first city:");
+        String city1 = input.next();
+        System.out.println("Please enter the second city:");
+        String city2 = input.next();
+        System.out.println("Please enter the third city:");
+        String city3 = input.next();
+    }
+}
