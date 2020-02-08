81ecdf7a085e0873ba3899ced782be7d49d67495
C-Wwb <1729220020@qq.com>
2020-02-08 17:40:21 +0800

    20.02.08

--- 4.24/src/Main.java
+++ 4.24/src/Main.java
@@ -11,5 +11,43 @@ public class Main
         String city2 = input.next();
         System.out.println("Please enter the third city:");
         String city3 = input.next();
+        if(city1.compareToIgnoreCase(city2) < 0)
+        {
+            if(city3.compareToIgnoreCase(city1) < 0)
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city3 + " " + city1 + " " + city2);
+            }
+            else if(city1.compareToIgnoreCase(city3) < 0 && city3.compareToIgnoreCase(city2) < 0)
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city1 + " " + city3 + " " + city2);
+            }
+            else
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city1 + " " + city2 + " " + city3);
+            }
+        }
+        else if(city2.compareToIgnoreCase(city1) < 0)
+        {
+            if(city3.compareToIgnoreCase(city2) < 0)
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city3 + " " + city2 + " " + city1);
+            }
+            else if(city2.compareToIgnoreCase(city3) < 0 && city3.compareToIgnoreCase(city1) < 0)
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city2 + " " + city3 + " " + city1);
+            }
+            else
+            {
+                System.out.println("The three cities in alphabetical order are "
+                        + city2 + " " + city1 + " " + city3);
+            }
+        }
+
     }
+
 }
