49af91452d71656fb0ff055410d99c73b7a065cc
C-Wwb <1729220020@qq.com>
2020-02-15 19:01:10 +0800

    20.02.15

--- 5.8/src/Main.java
+++ 5.8/src/Main.java
@@ -10,23 +10,19 @@ public class Main
         String[] name = new String[numberOfStudent];
         String theHighestScoreName = "";
         double[] grade = new double[numberOfStudent];
-        double theHightestScore = -1;
-       /* System.out.println("Please enter students name:");
+        double theHightestScore = grade[0];
+       System.out.println("Please enter students name and grades:");
         for(int i = 0; i < numberOfStudent; i++)
         {
             name[i] = input.next();
-        }*/
-        System.out.println("Please enter students grade:");
-        for(int i = 1; i < numberOfStudent; i++)
-        {
             grade[i] = input.nextDouble();
-            if(grade[i] > grade[i + 1])
+            if(grade[i] > theHightestScore)
             {
-                //theHighestScoreName = name[i + 1];
+                theHighestScoreName = name[i];
                 theHightestScore = grade[i];
             }
         }
         System.out.println("The highest score among the students was "
-                /*+ theHighestScoreName + " `s " */+ theHightestScore);
+                + theHighestScoreName + " `s " + theHightestScore);
     }
 }
