57853285e1bb70f96fef04b7fad02e91685030d4
C-Wwb <1729220020@qq.com>
2020-02-16 17:00:43 +0800

    20.02.16

---
+++ 5.11/src/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int optionedNumber = -1;
+        int count = 0;
+        System.out.println("The Numbers that are divisible by 5 and 6 are:");
+        for(int number = 100; number <= 1000; number++)
+        {
+
+            if(number % 5 == 0 || number % 6 == 0)
+            {
+                if(number % 5 == 0 && number % 6 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    optionedNumber = number;
+                    System.out.print(optionedNumber + " ");
+                    count++;
+                    if(count % 10 == 0)
+                    {
+                        System.out.println("");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
