905b78a3a4aa00327282c4edacd96edc97b0c3f5
C-Wwb <1729220020@qq.com>
2020-02-17 20:06:14 +0800

    20.02.17

--- DirectionInsertionSort/src/Main.java
+++ DirectionInsertionSort/src/Main.java
@@ -1,35 +1,28 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        /*Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a few Numbers:");
-        int[] chaoticArray = new int[100];//混乱数组
-        List<Integer> orderlyArray = new ArrayList<Integer>();//有序数组
-        for(int i = 0; i < 100; i++)
+
+        Scanner input = new Scanner(System.in);
+        int[] arr ={2,5,6,1,7};
+        int key = -1;
+        int j;
+        for(int i = 1; i < arr.length; i++)
         {
-            chaoticArray[i] = input.nextInt();//输入混乱数组中的数
-            orderlyArray.add(chaoticArray[0]);
-            if(chaoticArray[i] > orderlyArray.get(i))
+            if(arr[i - 1] > arr[i])
             {
-                orderlyArray.add(chaoticArray[i]);
+                key = arr[i];
+                for(j = i - 1; j >= 0 && arr[j] > key; j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+                arr[j + 1] = key;
             }
-
-        }*/
-
-        int [] arr = {3,2,5,8,6,4,9}; //插入排序
-        int i ,j , k;
-        for (i=1;i<arr.length;i++){
-            k = arr[i]; //待插入的元素
-            for (j=i-1;j>=0 && k<arr[j];j--){
-                arr[j+1] = arr[j];
-            }
-            arr[j+1]=k;
         }
         System.out.println(Arrays.toString(arr));
-
 
     }
 }
