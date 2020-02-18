5ec5d2ab3e116b6ae4790f201d4f61fee37ff4ed
C-Wwb <1729220020@qq.com>
2020-02-18 20:05:43 +0800

    20.02.18

--- 5.18(2)/src/Main.java
+++ 5.18(2)/src/Main.java
@@ -4,16 +4,14 @@ public class Main
     {
         for(int line = 1; line <= 6; line++)
         {
-            for(int number = 1; number <= line; number++)
+            for(int number = 1; number <= 7 - line; number++)
             {
                 System.out.print(number);
-                if(number == line)
+                if(number + line == 7)
                 {
                     System.out.println();
                 }
             }
-
-
         }
     }
 }
