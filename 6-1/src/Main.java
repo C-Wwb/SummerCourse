000fd8135cd5f905e83d65a092ed1937938d8aa7
C-Wwb <1729220020@qq.com>
2020-03-02 20:26:01 +0800

    20.03.02

---
+++ 6-1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number1 = 2;
+        int number2 = 5;
+        int max = Math.max(number1 , number2);
+        System.out.println("The maximum of 5 and 2 is: " + max);
+
+    }
+}
