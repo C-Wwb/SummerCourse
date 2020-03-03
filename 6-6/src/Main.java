1e8830804511dbb86b794cc86376d4a8eec05e65
C-Wwb <1729220020@qq.com>
2020-03-03 20:36:35 +0800

    20.03.03

--- 6-6/src/Main.java
+++ 6-6/src/Main.java
@@ -5,12 +5,15 @@ public class Main
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter first integer");
+        System.out.println("Please enter first integer: ");
         int firstInteger = input.nextInt();
-        System.out.println("Please enter second integer");
+        System.out.println("Please enter second integer: ");
         int secondInteger = input.nextInt();
-        long startTime = System.currentTimeMillis();
-        int gcd = 1;
+        GCD(firstInteger , secondInteger);
+    }
+    public static void GCD(int firstInteger, int secondInteger)
+    {
+        int gcd = 0;
         for(int k = 2; k /*<= firstInteger / 2 && k <= secondInteger / 2*/ > 0/*<= firstInteger && k <= secondInteger*/; k++)
         {
             if(firstInteger % k == 0 && secondInteger % k == 0)
@@ -19,9 +22,7 @@ public class Main
                 k++;
             }
         }
-        long finishTime = System.currentTimeMillis();
         System.out.println("The greatest common divisor for " + firstInteger + " and " + secondInteger
-            + " is " + gcd);
-        System.out.println("The project spend " + (finishTime - startTime) + " seconds");
+                + " is " + gcd);
     }
 }
