33f3a0abaa807ddac25da7f1e4326f11186e534e
C-Wwb <1729220020@qq.com>
2020-04-22 16:52:06 +0800

    20.04.22

--- Part8-二维数组/8-4/src/Main.java
+++ Part8-二维数组/8-4/src/Main.java
@@ -4,6 +4,49 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int[][] grid = readSolution();
+        System.out.println(isValid(grid) ? "Valid solution" : "Invalid solution");
+    }
+
+    public static boolean isValid(int[][] grid)
+    {
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++)
+                if(grid[i][j] < 1 || grid[i][j] > 9 || isValid(i, j, grid))
+                    return false;
+            return true;
+    }
+    public static boolean isValid(int i, int j, int[][] grid)
+    {
+        for(int col = 0; col < 9; col++)
+            if (col != j && grid[i][col] == grid[i][j])
+                return false;
+
+        for(int row = 0; row < 9; row++)
+            if (row != j && grid[i][row] == grid[i][j])
+                return false;
+
+        for(int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++)
+            for(int col = (i / 3) * 3; col < (i / 3) * 3 + 3; col++)
+            if (!(row == i && col == j) && grid[row][col] == grid[i][j])
+                return false;
+
+        return true;
+    }
+
+    private static int[][] readSolution()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a Sudoku puzzle solution: ");
+        int[][] grid = new int[9][9];
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++)
+                grid[i][j] = input.nextInt();
+        return grid;
+    }
+    /*
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a Sudoku puzzle solution: ");
         int row = 9;
@@ -62,5 +105,5 @@ public class Main
             i++;
         }
         return false;
-    }
+    }*/
 }
