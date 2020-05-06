ea5f1a97a9056945522dee43eced777311862677
C-Wwb <1729220020@qq.com>
2020-05-06 21:04:04 +0800

    20.05.06

---
+++ 8.18/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[][] m ={{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+        Collections.shuffle(Collections.singletonList(m));
+    }
+}
