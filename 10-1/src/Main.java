7e1f6f2c237d3d435adae9ad4a8f6bad269803a7
C-Wwb <1729220020@qq.com>
2020-06-02 17:52:59 +0800

    20.06.02

---
+++ 10-1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+}
+class Loan
+{
+    double annualInterestRate;
+    int numberOfYears;
+    double loanAmount;
+    java.util.Date loanDate;
+    Loan()
+    {
+        
+    }
+}
