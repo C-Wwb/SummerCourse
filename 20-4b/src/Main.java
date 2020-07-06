6d2783bbcc2d8e5f201b5c99fa4ba0ee90f29bb9
C-Wwb <1729220020@qq.com>
2020-07-06 19:35:27 +0800

    20

---
+++ 20-4b/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(1);
+        arrayList.add(4);
+        arrayList.add(0, 10);
+        arrayList.add(3, 30);
+        System.out.println(arrayList);
+
+        LinkedList<Object> linkedList = new LinkedList<>(arrayList);
+        linkedList.add(1, "red");
+        linkedList.removeLast();
+        linkedList.addFirst("green");
+        System.out.println(linkedList);
+
+        ListIterator<Object> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext())
+        {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+        while (listIterator.hasPrevious())
+        {
+            System.out.print(listIterator.previous() + " ");
+        }
+    }
+}
