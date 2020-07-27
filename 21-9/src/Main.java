af18b562886078edcd53b19501a3da099d908a31
C-Wwb <1729220020@qq.com>
2020-07-27 20:59:47 +0800

    20.07.27

---
+++ 21-9/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String text = "Good morning. Have a good class. "
+                + "Have a good visit. Have fun!";
+
+        Map<String, Integer> map = new TreeMap<>();
+        String[] words = text.split("[\\s+\\p{P}]");
+        for (String word : words) {
+            String key = word.toLowerCase();
+            if (!map.containsKey(key)) {
+                map.put(key, 1);
+            } else {
+                int value = map.get(key);
+                value++;
+                map.put(key, value);
+            }
+        }
+        map.forEach((key, value) -> System.out.println(key + "\t" + value));
+    }
+}
