4e1fb1f0fc2a3659250fb072874ffe925a4d3c7a
C-Wwb <1729220020@qq.com>
2020-08-06 19:40:39 +0800

    20.08.03

---
+++ 22-5/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Find all prime numbers <= n. Please enter n: ");
+        int n = input.nextInt();
+
+        final int NUMBER_PRE_LINE = 10;
+        int count = 0;
+        int number = 2;
+
+        System.out.println("The prime numbers are: ");
+        while (number <= n)
+        {
+            boolean isPrime = true;
+            for (int divisor = 2; divisor <= (int)(Math.sqrt(number)); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                count++;
+            }
+            if (count % NUMBER_PRE_LINE == 0)
+            {
+                System.out.printf("%7d\n", number);
+            }
+            else
+            {
+                System.out.printf("%7d", number);
+            }
+            number++;
+        }
+        System.out.println(count + " prime(s) less than or equal to " + n);
+    }
+}
