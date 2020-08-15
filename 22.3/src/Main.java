ef9cfb0423b641ca8a013ab2c7151c09e01afd88
C-Wwb <1729220020@qq.com>
2020-08-15 19:22:30 +0800

    20.08.15

---
+++ 22.3/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a string s1: ");
+        String s1 = input.nextLine();
+        System.out.println("Please enter a string s2: ");
+        String s2 = input.nextLine();
+        System.out.println(findIndex(s1, s2));
+    }
+    public static int findIndex(String s1, String s2)
+    {
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < s1.length())
+        {
+            if (s1.charAt(i) == s2.charAt(j))
+            {
+                if (j == 0)
+                {
+                    index = i;
+                }
+                i++;
+                j++;
+            }
+            else
+            {
+                i += 1 - j;
+                j = 0;
+            }
+            if (j == s2.length())
+            {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
+/*
+text
+mississippi
+sip
+
+welcome to java
+come
+ */
