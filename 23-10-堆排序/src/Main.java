afa5ff3a3fb28fe28bfe627d71e873e0f5f451c7
C-Wwb <1729220020@qq.com>
2020-08-30 19:33:17 +0800

    20.08.30

--- 23-10-堆排序/src/Main.java
+++ 23-10-堆排序/src/Main.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static  void main(String[] args)
     {
-
+        Integer[] list = {0, -1, 2, 9, 5 ,4};
+        HeapSort.heapSort(list);
+        System.out.println(Arrays.toString(list));
+    }
+}
+class HeapSort
+{
+    public static <E extends Comparable<E>> void heapSort(E[] list)
+    {
+        Heap<E> heap = new Heap<E>();
+        for (int i = 0; i < list.length; i++)
+        {
+            heap.add(list[i]);
+        }
+        for (int i = list.length - 1; i >= 0; i--)
+        {
+            list[i] = heap.remove();
+        }
     }
 }
 class Heap<E extends Comparable<E>>
